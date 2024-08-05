79d36f8e05da87d02c22080689d6455e3ed87152
1Qlayd <kostya.pashkov.90@mail.ru>
2024-08-05 12:38:34 +0500

    valid-palindrome solution

---
+++ 125_VaildPalindrome/valid-palindrome.js
@@ -0,0 +1,33 @@
+const isPalindrome = (s) => {
+    str = []
+
+    for ( i = 0; i < s.length; i++) {
+
+        const char = s[i].toLowerCase()
+
+        if (char !== "!" && char !== "," && char !== "." && char !== "?" && 
+            char !== ":" && char !== ";" && char !== ")" && char !== "(" &&
+            char !== "'" && char !== "-" && char !== "[" && char !== "]" && 
+            char !== " " && char !== "@" && char !== "#" && char !== "№" && 
+            char !== "_" && char !== "/" && char !== "|") {
+            str.push(char)
+        } 
+
+    }
+
+    const readyStr = str.join('')
+    const reversedStr = readyStr.split('').reverse().join('');
+
+    if (readyStr === reversedStr) {
+
+        //console.log(true);
+        return(true)
+    } else {
+
+        return(false)
+        //console.log(false);
+    }
+
+}
+
+isPalindrome("A man, a plan, a canal: Panama")
